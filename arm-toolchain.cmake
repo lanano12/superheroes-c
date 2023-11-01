e22b92868e98270e68afba830bd416076001b51b
Lee Ainsley <lee_ainsley@msn.com>
2023-11-01 21:20:41 +0000

    Update

--- arm-toolchain.cmake
+++ arm-toolchain.cmake
@@ -5,9 +5,6 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 # Specify the cross compiler
 set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
 
-# Where is the target environment
-set(CMAKE_FIND_ROOT_PATH /path/to/arm/sysroot)
-
 # Adjust the default behavior of the FIND_XXX() commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
